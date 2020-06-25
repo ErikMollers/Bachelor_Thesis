0bc117195956824c83c73b4f503965181d7f52e7
ErikMollers <erik.mollers@hotmail.com>
2020-06-25 18:21:33 +0200

    Added the results for the AC angular frequency of 5e2.

--- Data/saltConcentration_timeDep/24-06-2020_ErmsGraph/The script for the Graph.py
+++ Data/saltConcentration_timeDep/24-06-2020_ErmsGraph/The script for the Graph.py
@@ -8,14 +8,14 @@ Kappa = L_Debye**-1
 Length = 290*10**-9
 Diff = 10**-9
 Phi = 0.025
-Omega = [100, 400*math.pi, 10000, 104719.7551, 1324169.717]
+Omega = [100, 500, 400*math.pi, 10000, 104719.7551, 1324169.717]
 
 E_theory = [(i*Phi*(Length*Kappa + 2*math.sqrt(Length*Kappa)))/(math.sqrt(2)*Length*Kappa*(i*Length + 2*Diff*Kappa)) for i in Omega]
-E_Numerical = [8.391182825666391, 105.0471501568999, 836.5720023805205, 8232.105499029609, 54772.480486267865]
+E_Numerical = [8.391182825666391, 41.80725981993905, 105.0471501568999, 836.5720023805205, 8232.105499029609, 54772.480486267865]
 print(E_theory)
 
 fig, ax = plt.subplots(figsize=(5, 5))
-ax.plot(Omega, E_theory, 'k--', label=('Theorethical'))
+ax.plot(Omega, E_theory, 'k--', label=('Theoretical'))
 ax.plot(Omega, E_Numerical, 'r', label=('Numerical'))
 ax.legend()
 ax.set_xscale("log")
