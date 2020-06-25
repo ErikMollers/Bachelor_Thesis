ffcc360492036e963c045b364e35e681133abdb0
ErikMollers <erik.mollers@hotmail.com>
2020-06-25 12:57:10 +0200

    added the AC result of the angular frequency = e-5

--- Data/saltConcentration_timeDep/24-06-2020_ErmsGraph/The script for the Graph.py
+++ Data/saltConcentration_timeDep/24-06-2020_ErmsGraph/The script for the Graph.py
@@ -8,8 +8,16 @@ Kappa = L_Debye**-1
 Length = 290*10**-9
 Diff = 10**-9
 Phi = 0.025
-Omega = [100, 1000, 10000, 100000, 1300000]
+Omega = [100, 10000, 104719.7551, 1324169.717]
 
 E_theory = [(i*Phi*(Length*Kappa + 2*math.sqrt(Length*Kappa)))/(math.sqrt(2)*Length*Kappa*(i*Length + 2*Diff*Kappa)) for i in Omega]
-
+E_Numerical = [8.391182825666391, 836.5720023805205, 8232.105499029609, 54772.480486267865]
 print(E_theory)
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.plot(Omega, E_theory, 'k--', label=('Theorethical'))
+ax.plot(Omega, E_Numerical, 'r', label=('Numerical'))
+ax.legend()
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlabel('$\omega$')
+ax.set_ylabel('$E_{rms}$ $(V/m)$')
