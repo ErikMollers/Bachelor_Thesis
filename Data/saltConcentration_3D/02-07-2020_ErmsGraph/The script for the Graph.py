5d5b19f66938b9b01dbd4d247912e83a3938bb6f
ErikMollers <erik.mollers@hotmail.com>
2020-07-03 10:14:19 +0200

    Added the results for the 3D case for an angular frequency of e2.

---
+++ Data/saltConcentration_3D/02-07-2020_ErmsGraph/The script for the Graph.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import scipy.constants as sp
+
+L_Debye = math.sqrt((sp.R*300*80*sp.epsilon_0)/(2*(sp.e*sp.Avogadro)**2))
+Kappa = L_Debye**-1 
+Length = 290*10**-9
+Diff = 10**-9
+Phi = 0.025
+Omega = [100, ]
+
+E_theory = [(i*Phi*(Length*Kappa + 2*math.sqrt(Length*Kappa)))/(math.sqrt(2)*Length*Kappa*(i*Length + 2*Diff*Kappa)) for i in Omega]
+E_Numerical = [7.810283419189252, ]
+print(E_theory)
+
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.plot(Omega, E_theory, 'k--', label=('Theoretical'))
+ax.plot(Omega, E_Numerical, 'r', label=('Numerical'))
+ax.legend()
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlabel('$\omega$')
+ax.set_ylabel('$E_{rms}$ $(V/m)$')
